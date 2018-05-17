700cd1aa267c141f1a0999f9bbcdc29633851387
Min-Hua Ivy Chen <mhichen@umich.edu>
2018-05-16 19:15:28 -0500

    Refreshing on some tensorflow basics.

--- TensorFlow/tensor_flow_basics.py
+++ TensorFlow/tensor_flow_basics.py
@@ -1,9 +1,38 @@
+#!/usr/bin/python3
+
 import time
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-
+import tensorflow as tf
         
 if __name__ == "__main__":
 
+    x = tf.Variable(3, name = "x")
+    y = tf.Variable(4, name = "y")
+    f = x * x * y + y + 2
 
+    # One way to run a session
+    sess = tf.Session()
+    sess.run(x.initializer)
+    sess.run(y.initializer)
+    result = sess.run(f)
+    print(result)
+
+    sess.close()
+
+
+    # Another way to run a session
+    with tf.Session() as sess:
+        x.initializer.run()
+        y.initializer.run()
+        result = f.eval()
+        print(result)
+        
+
+    # Another way to do things
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        init.run()
+        result = f.eval()
